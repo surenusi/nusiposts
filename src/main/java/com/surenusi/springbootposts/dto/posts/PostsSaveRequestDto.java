7352ec4ed44701f4fcf70f0a0c860541b2ee29f9
surenusi <mixma789@gmail.com>
2022-12-20 17:10:29 +0900

    <TEST> 테스트를 위한 생성자 추가

--- src/main/java/com/surenusi/springbootposts/dto/posts/PostsSaveRequestDto.java
+++ src/main/java/com/surenusi/springbootposts/dto/posts/PostsSaveRequestDto.java
@@ -11,6 +11,13 @@ public class PostsSaveRequestDto {
     private String content;
     private String author;
 
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
     public Posts toEntity() {
         return Posts.builder()
                 .title(title)
